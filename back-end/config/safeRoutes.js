edc48f86dcfa177e953f549f65310c7b680a9d62
ouieqare <esther.e@ouieqare.com>
2024-07-23 23:43:58 +0300

    Update MongoDB connection settings for production

--- back-end/config/safeRoutes.js
+++ back-end/config/safeRoutes.js
@@ -1,38 +1,68 @@
+// const jwt = require('jsonwebtoken');
+// const config = require('./keys');
+// const User = require('../models/user');
+
+// const reqAuth = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   console.log('Received authorization header:', authHeader);
+
+
+//   // Vérifier que l'en-tête existe
+//   if (!authHeader) {
+//     return res.status(401).json({ success: false, msg: 'No authorization token provided' });
+//   }
+
+//   // Extraire le token
+//   const tokenParts = authHeader.split(' ');
+// if (tokenParts.length !== 2 || (tokenParts[0] !== 'Bearer' && tokenParts[0] !== 'JWT')) {
+//     return res.status(401).json({ success: false, msg: 'Token format is invalid' });
+// }
+
+
+// const token = tokenParts[1];
+//     console.log('Token being verified:', token);
+//     jwt.verify(token, config.secret, (err, decoded) => {
+//         if (err) {
+//             console.log('Token verification error:', err);
+//             return res.status(401).json({ success: false, msg: 'Token is invalid' });
+//         }
+//         req.user = decoded; // Assurez-vous que vous définissez req.user ici
+//         next();
+//     });
+
+// };
+
+
+// module.exports = {
+//    reqAuth: reqAuth
+// };
+
 const jwt = require('jsonwebtoken');
 const config = require('./keys');
-const User = require('../models/user');
 
 const reqAuth = (req, res, next) => {
   const authHeader = req.headers.authorization;
   console.log('Received authorization header:', authHeader);
 
-
-  // Vérifier que l'en-tête existe
   if (!authHeader) {
-    return res.status(401).json({ success: false, msg: 'No authorization token provided' });
+    return res.redirect('/login');
   }
 
-  // Extraire le token
   const tokenParts = authHeader.split(' ');
-if (tokenParts.length !== 2 || (tokenParts[0] !== 'Bearer' && tokenParts[0] !== 'JWT')) {
-    return res.status(401).json({ success: false, msg: 'Token format is invalid' });
-}
+  if (tokenParts.length !== 2 || (tokenParts[0] !== 'Bearer' && tokenParts[0] !== 'JWT')) {
+    return res.redirect('/login');
+  }
 
-
-const token = tokenParts[1];
-    console.log('Token being verified:', token);
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-            console.log('Token verification error:', err);
-            return res.status(401).json({ success: false, msg: 'Token is invalid' });
-        }
-        req.user = decoded; // Assurez-vous que vous définissez req.user ici
-        next();
-    });
-
+  const token = tokenParts[1];
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      console.log('Token verification error:', err);
+      return res.redirect('/login');
+    }
+    req.user = decoded;
+    next();
+  });
 };
 
+module.exports = { reqAuth };
 
-module.exports = {
-   reqAuth: reqAuth
-};

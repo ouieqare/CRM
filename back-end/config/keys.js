9cc2ea9f3860f8f758d6ce1bf774da6cd273601d
ouieqare <esther.e@ouieqare.com>
2024-07-23 22:44:28 +0300

    Update MongoDB connection settings for production

--- back-end/config/keys.js
+++ back-end/config/keys.js
@@ -1,23 +1,10 @@
 dbPasswordDev ='mongodb://localhost/Ouieqare';
 
-// for PRODUCTION
-// const MONGO_USERNAME = 'esthere';
-// const MONGO_PASSWORD = 'Nb3UhA8heygqwENV';
-// const MONGO_HOSTNAME = 'cluster0.ygczhra.mongodb.net';
-// // const MONGO_PORT = '27017';
-// const MONGO_DB = 'ouieqare';
-
-// const dbPasswordProd = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}?retryWrites=true&w=majority`;
+const dbPasswordProd = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB}?retryWrites=true&w=majority`;
 // // const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-// const mongoURI = process.env.NODE_ENV === 'production' ? dbPasswordProd : dbPasswordDev;
+const mongoURI = process.env.NODE_ENV === 'production' ? dbPasswordProd : dbPasswordDev;
 
 module.exports = {
-  mongoURI: dbPasswordDev,
+  mongoURI: mongoURI,
   secret: 'yourSecretKey',
 };
-
-// module.exports = {
-//   secret: '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',
-//   googleClientID: '372588693859-odccs5brht2mlo6e2857lbhrlipql13m.apps.googleusercontent.com',
-//   googleClientSecret: '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'
-// };

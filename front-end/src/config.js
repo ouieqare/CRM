5cd3d99daef1d69286e8cf2ad8c0d519786cc663
ouieqare <esther.e@ouieqare.com>
2024-07-24 12:14:44 +0300

    config.js

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,7 +1,14 @@
+// const config = {
+//     WS_BASE_URL: 'http://localhost:5100/api/',
+//     DOMAIN_NAME: 'http://localhost:3000',
+//     DEMO: true
+// }
+
+// export default config;
 const config = {
-    WS_BASE_URL: 'http://localhost:5100/api/',
-    DOMAIN_NAME: 'http://localhost:3000',
+    WS_BASE_URL: 'https://ouieqare-crm.herokuapp.com/api/',  // Remplacez par l'URL de votre application Heroku
+    DOMAIN_NAME: 'https://ouieqare-crm.herokuapp.com',  // Remplacez par l'URL de votre application Heroku
     DEMO: true
-}
+};
 
 export default config;

855b960d7aabdb8ca757bfcefa1e719d8e421f4f
ouieqare <esther.e@ouieqare.com>
2024-07-28 14:38:46 +0300

    table

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -52,7 +52,7 @@ var routes = [
   },
   {
     path: "/tables",
-    name: "Tables",
+    name: "Statuts Clients",
     icon: "ni ni-bullet-list-67 text-red",
     component: Tables,
     layout: "/admin",

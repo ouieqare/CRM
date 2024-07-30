e9e4f5b7ab47a46da15441d5a9eab90a8f4ed3f5
ouieqare <esther.e@ouieqare.com>
2024-07-30 14:22:44 +0300

    header nmbr clients

--- front-end/src/views/examples/ClientsContext.js
+++ front-end/src/views/examples/ClientsContext.js
@@ -5,13 +5,21 @@ const ClientsContext = createContext();
 export const useClients = () => useContext(ClientsContext);
 
 export const ClientsProvider = ({ children }) => {
-  const [totalClients, setTotalClients] = useState(0);
-
-  // Ici, vous devriez mettre à jour `totalClients` en fonction de votre logique d'application
-
-  return (
-    <ClientsContext.Provider value={{ totalClients, setTotalClients }}>
-      {children}
-    </ClientsContext.Provider>
-  );
-};
+    const [totalClients, setTotalClients] = useState(0);
+  
+    // Vous pouvez également ajouter des fonctions pour manipuler totalClients de manière plus contrôlée ici
+    const incrementClients = (amount = 1) => {
+      setTotalClients(prev => prev + amount);
+    };
+  
+    const decrementClients = (amount = 1) => {
+      setTotalClients(prev => prev - amount);
+    };
+  
+    return (
+      <ClientsContext.Provider value={{ totalClients, setTotalClients, incrementClients, decrementClients }}>
+        {children}
+      </ClientsContext.Provider>
+    );
+  };
+  
